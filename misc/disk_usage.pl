a3d6acd35fdf7a6fef02b0171493b355076786a4
eribertto_Fed43_T460p <erimendz@gmail.com>
2026-03-30 17:11:40 +0300

    add modern perl pragma and replace hardcoded user home dir with a portable one

--- misc/disk_usage.pl
+++ misc/disk_usage.pl
@@ -1,8 +1,19 @@
 #!/usr/bin/env perl
 
-use v5.40;
-use strict;
-use warnings;
+use Modern::Perl '2025';
 
-my $disk = qx(df -h /home/eriberttom);
+#use strict;
+#use warnings;
+
+my $user = qx(whoami);
+
+#say "Your username is $user";
+
+# get the full user home dir
+my $home = '/home/' . $user;
+
+#say "Your home directory is $home";
+
+# say my $disk = qx(df -h /home/eriberttom);
+say my $disk = qx(df -h $home);
 say "Disk usage:\n$disk";
